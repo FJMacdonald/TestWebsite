ed6dcb3cd62173f91bffaf3f4131d5413d39f4be
Francesca Macdonald <francescamacdonald@rogers.com>
2024-04-04 14:55:29 -0400

    some improvements to rank chart

--- myblog/js/rankChart.js
+++ myblog/js/rankChart.js
@@ -3,12 +3,12 @@ function drawRankChart(athletesData, colorPalette) {
     console.log(athletesData);
 
     // Define chart dimensions
-    const margin = { top: 20, right: 20, bottom: 20, left: 10 };
+    const margin = { top: 20, right: 30, bottom: 20, left: 10 };
     const width = window.innerWidth - margin.left - margin.right;
-    const height = 0.6 * window.innerHeight;
+    const height = 17 * athletesData.length;
 
     //get the section lengths 
-    const sectionLength = width / 12;
+    const sectionLength = width / 8;
 
     let chartTitleDiv = document.getElementById("rankchart_title");
 
@@ -44,30 +44,39 @@ function drawRankChart(athletesData, colorPalette) {
     const yScale = d3.scaleLinear()
         .domain([1, athletesData.length]) // Rank starts from 1
         .range([margin.top, height - margin.bottom]);
-
+    const sectionEnds = [
+        xScale(0.2),
+        xScale(1),
+        xScale(1.2),
+        xScale(4),
+        xScale(4.2),
+        xScale(5.2),
+        xScale(5.8),
+    ];
     // Draw lines connecting swim, bike, run, and finish positions for each athlete
     const athleteLines = svg.selectAll('.athlete-line')
         .data(athletesData)
         .enter()
         .append('path')
         .attr('class', 'athlete-line')
-        .attr('d', d => { if (d.status == '' ){
-            const startX = xScale(0.2);
-            const startY = yScale(d.swim_rank);
-            const swimX = xScale(0.5) + sectionLength;
-            const swimY = yScale(d.swim_rank);
-            const t1X = xScale(1) + sectionLength;
-            const t1Y = yScale(d.t1_rank);
-            const bikeX = xScale(2) + sectionLength * 2;
-            const bikeY = yScale(d.bike_rank);
-            const t2X = xScale(3) + sectionLength;
-            const t2Y = yScale(d.t2_rank);
-            const runX = xScale(4) + sectionLength;
-            const runY = yScale(d.run_rank);
-            const finishX = xScale(4.8) + sectionLength;
-            const finishY = yScale(d.run_rank);
-        
-            return `M${startX},${startY} L${swimX},${swimY} L${t1X},${t1Y} L${bikeX},${bikeY} L${t2X},${t2Y} L${runX},${runY} L${finishX},${finishY}`;
+        .attr('d', d => {
+            if (d.status == '') {
+                const startX = sectionEnds[0];//start
+                const startY = yScale(d.swim_rank);
+                const swimX = sectionEnds[1];//swim
+                const swimY = yScale(d.swim_rank);
+                const t1X = sectionEnds[2];//t1
+                const t1Y = yScale(d.t1_rank);
+                const bikeX = sectionEnds[3];//bike
+                const bikeY = yScale(d.bike_rank);
+                const t2X = sectionEnds[4];//t2
+                const t2Y = yScale(d.t2_rank);
+                const runX = sectionEnds[5];//run
+                const runY = yScale(d.run_rank);
+                const finishX = sectionEnds[6];
+                const finishY = yScale(d.run_rank);
+
+                return `M${startX},${startY} L${swimX},${swimY} L${t1X},${t1Y} L${bikeX},${bikeY} L${t2X},${t2Y} L${runX},${runY} L${finishX},${finishY}`;
             }
         })
         .attr('stroke', (d, i) => colorPalette[i])
@@ -76,10 +85,10 @@ function drawRankChart(athletesData, colorPalette) {
 
     // Highlight athlete path on hover or tap
     athleteLines.on('mouseover', function (event, d) {
-            d3.select(this).attr('stroke-width', 5);
-            // Bold the associated athlete name label
-            svg.select(`#athlete-label-${d.athleteName.replace(/\s/g, '')}`).style('font-weight', 'bold');
-        })
+        d3.select(this).attr('stroke-width', 5);
+        // Bold the associated athlete name label
+        svg.select(`#athlete-label-${d.athleteName.replace(/\s/g, '')}`).style('font-weight', 'bold');
+    })
         .on('mouseout', function (event, d) {
             d3.select(this).attr('stroke-width', 2);
             // Revert the associated athlete name label to normal
@@ -96,33 +105,37 @@ function drawRankChart(athletesData, colorPalette) {
             svg.select(`#athlete-label-${d.athleteName.replace(/\s/g, '')}`).style('font-weight', 'normal');
         });
 
-    // Add swim, bike, run labels
-    const labels = ['Swim', 'T1', 'Bike', 'T2', 'Run', 'Finish'];
-    svg.selectAll('.race-label')
-        .data(labels)
+
+
+    // Calculate section labels
+    const sectionLabels = ['Swim', 'T1', 'Bike', 'T2', 'Run', 'Finish'];
+
+    // Add section labels
+    svg.selectAll('.section-label')
+        .data(sectionLabels)
         .enter()
         .append('text')
-        .attr('class', 'label race-label')
-        .attr('x', (d, i) => xScale(i) + sectionLength / 2)
-        .attr('y', height - 10)
+        .attr('class', 'label section-label')
+        .attr('x', (d, i) => {
+            return (sectionEnds[i] + sectionEnds[i + 1]) / 2; // Midpoint between section ends
+        })
+        .attr('y', height - margin.bottom + 15) // Adjust for label position below the x-axis
         .attr('text-anchor', 'middle')
         .text(d => d)
-        .attr('fill', 'black')
-        // .style('font-size', '12px')
-        .style('font-weight', 'bold');
+        .attr('fill', 'black');
 
     // Add swim ranks on the left
     svg.selectAll('.swim-rank-label')
         .data(athletesData)
         .enter()
         .append('text')
-        .attr('class', 'label swim-rank-label')
+        .attr('class', 'label-small swim-rank-label')
         .attr('x', margin.left - 5)
         .attr('y', d => yScale(d.swim_rank))
         .attr('dy', '0.35em')
         .text(d => d.swim_rank)
         .attr('fill', 'black')
-        // .style('font-size', '12px');
+    // .style('font-size', '12px');
 
     // Add athlete names at the finish position
     svg.selectAll('.athlete-label')
@@ -131,24 +144,23 @@ function drawRankChart(athletesData, colorPalette) {
         .append('text')
         .attr('class', 'label athlete-label')
         .attr('id', d => `athlete-label-${d.athleteName.replace(/\s/g, '')}`) // Add unique IDs to the labels    
-        .attr('x', xScale(4.8) + sectionLength)
+        .attr('x', xScale(5.8))
         .attr('y', d => yScale(d.run_rank))
         .attr('dy', '0.35em')
         .text(d => d.status != '' ? '' : d.athleteName)
         .attr('fill', (d, i) => colorPalette[i])
-        // .style('font-size', '14px');
+    // .style('font-size', '14px');
 
 
-    // Add vertical gridlines for each section
-    const sectionEnds = [1.13, 1.63, 3.25, 3.63, 4.63]; // x-values for section ends
+
     svg.selectAll('.section-line')
         .data(sectionEnds)
         .enter()
         .append('line')
         .attr('class', 'section-line')
-        .attr('x1', d => xScale(d)) // Adjust x-position for half-length swim section
+        .attr('x1', d => d)
         .attr('y1', margin.top)
-        .attr('x2', d => xScale(d)) // Adjust x-position for half-length swim section
+        .attr('x2', d => d)
         .attr('y2', height - margin.bottom)
         .attr('stroke', 'lightgray')
         .attr('stroke-width', 1)
